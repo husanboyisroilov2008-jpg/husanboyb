0903585aa252a38264e454ec7b798e52f5ee4641
husanboyisroilov2008-jpg <husanboyisroilov2008@gmail.com>
2026-04-05 10:38:16 +0500

    Update admin-overview.js

--- netlify/functions/admin-overview.js
+++ netlify/functions/admin-overview.js
@@ -1,18 +1,26 @@
+const { connectLambda } = require('@netlify/blobs');
 const { methodNotAllowed, ok, serverError, unauthorized } = require('./_lib/response');
 const { requireAdmin } = require('./_lib/auth');
 const { getUsers, getCampaigns, computeStats } = require('./_lib/storage');
 
 exports.handler = async function handler(event) {
   try {
+    connectLambda(event);
+
     if (event.httpMethod !== 'GET') return methodNotAllowed(['GET']);
+
     const admin = requireAdmin(event);
     if (!admin) return unauthorized('Admin sessiya topilmadi.');
 
     const users = await getUsers();
     const campaigns = await getCampaigns();
 
-    const sortedUsers = [...users].sort((a, b) => new Date(b.joinedAt) - new Date(a.joinedAt));
-    const sortedCampaigns = [...campaigns].sort((a, b) => new Date(b.sentAt) - new Date(a.sentAt));
+    const sortedUsers = [...users].sort(
+      (a, b) => new Date(b.joinedAt) - new Date(a.joinedAt)
+    );
+    const sortedCampaigns = [...campaigns].sort(
+      (a, b) => new Date(b.sentAt) - new Date(a.sentAt)
+    );
 
     return ok({
       admin,
